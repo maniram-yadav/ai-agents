4e9e1d2815483489033d20981caa1051bd1220a3
maniram <maniramyadav786@gmail.com>
2025-04-23 07:43:19 +0530

    marketing content creator

--- market_researcher/digital_marketer.py
+++ market_researcher/digital_marketer.py
@@ -1,12 +1,11 @@
 from crewai import Agent
 
-def getMarketResearcher(llm) :
-    market_researcher = Agent(
-        role='Market researcher',
-        goal = '',
-        backstory = '',
-        verbose = True,
-        allow_delegation=False,
-        llm = llm
+def getDigitalMarketingContentCreator(llm) :
+    digital_marketer =  Agent(
+    role = 'Digital Marketing Content Creator',
+    goal='Come up with 2 or 3 interesting advertisement ideas for marketing on digital platforms such as Youtube, Instagram amd Tiktok along with script for each marketing campaign',
+    backstory = 'You are a marketing marketer specialising in performance marketing in the pet product industry',
+    verbose= True,
+    allow_delegation= False,
+    llm=llm
     )
-    return market_researcher
